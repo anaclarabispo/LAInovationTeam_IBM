be8714fd384eb88ffb812e26658f45cb6828ae54
Ana Clara Bispo <33198356+anaclarabispo@users.noreply.github.com>
2019-05-14 11:40:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 /*  LA Inovation Team - IBM        */
-/*  Data: 13/05/2019               */ 
+/*  Data: 13/05/2019               */
 /*  Name: Ana Clara Emanulle Bispo */
 
 var funcName = ["John Doe", "Peter Parker", "Mary Jane WatsonParker", "James Doe", "John Elvis Doe", "Jane Doe", "Penny Parker"];
-var nomeMail  = [];
-var arrayMail = [];
-var repetidos = [];
+var nomeMail = [];
 var email = [];
 var aux = [];
 var id = 0;
+var namesMap = new Map();
 
 /* Compõe o nome inicial do e-mail (Seleção de Letras e Nomes) */
 const priLetra = funcName.map(([v]) => v);
@@ -23,40 +22,60 @@ const ultiName = funcName.map(name => {
   return name.split(' ').slice(-1);
 });
 
-var mails = {"priLetra": priLetra, "segLetra": segLetra, "ultiName": ultiName};
+if (((funcName[2] == "Mary Jane WatsonParker") || (funcName[4] == "John Elvis Doe"))) {
+  var mails1 = {
+    "priLetra": priLetra, "segLetra": segLetra, "ultiName": ultiName
+  };
+} else {
+  var mails2 = {
+    "priLetra": priLetra, "ultiName": ultiName
+  };
+}
 
 /* Gera nome inicial do e-mail antes do @ */
-function montaMail(mails) {
-    for (const j in mails.ultiName) {
-      if (((funcName[j] == "Mary Jane WatsonParker") || (funcName[j] == "John Elvis Doe"))) {
-          aux.push(mails.ultiName[j] + "." + mails.priLetra[j] + "." + mails.segLetra[j]);
-      } else {
-          aux.push(mails.ultiName[j] + "." + mails.priLetra[j]);
-      }
+function montaMail(mails1, mails2) {
+  for (const j in mails1.ultiName) {
+    if (((funcName[j] == "Mary Jane WatsonParker") || (funcName[j] == "John Elvis Doe"))) {
+      aux.push(mails1.ultiName[j] + "." + mails1.priLetra[j] + "." + mails1.segLetra[j]);
+    } else {
+      aux.push(mails1.ultiName[j] + "." + mails1.priLetra[j]);
     }
-    return aux
   }
-
-nomeMail = montaMail(mails);
+  return aux
+}
+nomeMail = montaMail(mails1);
 
 /* Consolida o Nome + Dominio e verifica duplicidade de e-mails */
 function consolidaMail(nomeMail) {
+
   for (var i = 0; i < nomeMail.length; i++) {
-    if (nomeMail.indexOf(nomeMail[i]) !== nomeMail.lastIndexOf(nomeMail[i])) {
-      id = id + 1; 
-      email.push(nomeMail[i] + id + "@company.com");
-    } else {
-      email.push(nomeMail[i] + "@company.com");
-    }
+    checkDuplicity(nomeMail[i]);
   }
   return email;
 }
 
+/* Valida duplicidade de e-mails*/
+function checkDuplicity(nome) {
+  var indice = namesMap.get(nome + "@company.com");
+  if (indice == null) {
+    namesMap.set(nome + "@company.com", 0);
+    email.push(nome + "@company.com")
+  }
+  if (indice >= 0) {
+    indice++;
+    if (namesMap.get(nome + indice + "@company.com") > 0) {
+      indice++;
+    }
+    namesMap.set(nome + indice + "@company.com", indice);
+    email.push(nome + indice + "@company.com")
+  }
+}
+
 /* Impressão - Output */
 console.log('                Lista de E-mail Criados - Dominio Company                ');
-for(var i = 0; i< funcName.length; i++){
-  if(i == funcName.length -1 ){
-    console.log(funcName[i] + " < " + consolidaMail(nomeMail)[i] + " >. ");    
+for (var i = 0; i < funcName.length; i++) {
+  if (i == funcName.length -1) {
+    console.log(funcName[i] + " < " + consolidaMail(nomeMail)[i] + " >. ");
   } else {
     console.log(funcName[i] + " < " + consolidaMail(nomeMail)[i] + " >, ");
   }
